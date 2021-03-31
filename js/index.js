8038529f76f7f6e2f84c1b0bfa64dffa3e6fd002
Information-Services <81656794+InSer-Information-Services@users.noreply.github.com>
2021-03-30 18:44:54 -0600

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,14 @@
+document.querySelector('.menu-btn').addEventListener('click', () => {
+    document.querySelector('.nav-menu').classList.toggle('show')
+})
+
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.cards-banner-one',{delay:200});
+ScrollReveal().reveal('.servicios',{delay:200});
+ScrollReveal().reveal('.new-cards',{delay:200});
+ScrollReveal().reveal('.new-cards2',{delay:200});
+ScrollReveal().reveal('.new-cards3',{delay:200});
+ScrollReveal().reveal('.social',{delay:200});
+ScrollReveal().reveal('.footer',{delay:200});
+
